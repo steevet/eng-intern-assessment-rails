de4432d793c06b98e2669a87e619c5e78dd116e6
steevet <ststeeve067@gmail.com>
2024-01-29 23:59:05 -0500

    yes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,44 @@
-class ApplicationController < ActionController::Base
-end
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @articles = Article.all
+    end
+  
+    def new
+      @article = Article.new
+    end
+  
+    def create
+      @article = Article.new(article_params)
+  
+      if @article.save
+        redirect_to @article, notice: 'Article is successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def update
+      if @article.update(article_params)
+        redirect_to @article, notice: 'Article is successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @article.destroy
+      redirect_to articles_url, notice: 'Article is successfully destroyed.'
+    end
+  
+    private
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
+  
+    def article_params
+      params.require(:article).permit(:title, :content, :author, :date)
+    end
+  end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
-class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+# class ApplicationRecord < ActiveRecord::Base
+#   primary_abstract_class
+# end
+
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def self.search(query)
+    where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+  end
 end
